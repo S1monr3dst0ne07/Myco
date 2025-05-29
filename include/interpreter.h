e74c82bc8d273d6705b83756ea5d597ca8c70117
Ivy Banana <96400046+TrendyBananaYT@users.noreply.github.com>
2025-05-29 00:23:43 -0400

    Delete include directory

--- include/interpreter.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYCO_INTERPRETER_H
-#define MYCO_INTERPRETER_H
-
-#include "parser.h"
-
-// Function to interpret the entire AST
-int64_t interpret_ast(ASTNode* ast);
-
-#endif // MYCO_INTERPRETER_H 
