7def36b40419c6e5c83084c45067ec4b6429a212
Ivy Banana <cnnorwood14@gmail.com>
2025-08-15 15:27:45 -0400

    Recommitting

--- include/codegen.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODEGEN_H
-#define CODEGEN_H
-
-#include "parser.h"
-
-// Function prototypes
-int codegen_generate(ASTNode* ast, const char* filename, int keep_output);
-
-#endif // CODEGEN_H 
